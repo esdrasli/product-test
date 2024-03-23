648d9a6afc41f0427cb154444840a0f022be8bf5
esdrasli <esdrasli@gmail.com>
2024-03-23 00:26:29 -0300

    Add: Adjust name

--- product-frontend/src/components/Header.js
+++ product-frontend/src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
     <header style={{ backgroundColor: '#333', color: '#fff', padding: '20px 0', borderBottom: '2px solid #ccc' }}>
       <nav>
         <ul style={{ display: 'flex', justifyContent: 'center', listStyle: 'none', padding: 0 }}>
-          <h1 style={{ margin: 0, fontFamily: 'Arial, sans-serif', fontSize: '24px' }}>
-            Natanael Store
+          <h1 style={{ margin: 0, fontFamily: 'Arial, sans-serif', fontSize: '32px' }}>
+            Cadastro de Produtos
           </h1>
         </ul>
       </nav>
